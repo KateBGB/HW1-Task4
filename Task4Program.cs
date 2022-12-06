1bf034245e082b53b187531e2d42e83ab7bcc39a
Ekaterina Biyushkina <biyushkina.k@gmail.com>
2022-12-06 15:46:15 +0300

    Programe created

---
+++ Task4Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите первое число:() ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число:() ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число:() ");
+int c = int.Parse(Console.ReadLine());
+int maximal = a;
+if(b>a)
+{
+    maximal = b;
+}
+else 
+{
+    if(c>a)
+    {
+
+        maximal = c;
+    }
+    else maximal = a;
+}
+Console.WriteLine($"Число {maximal} является наибольшим из 3 введенных");
